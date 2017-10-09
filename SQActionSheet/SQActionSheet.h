08d674477d20caa8fc9fb12a4741f346a8765326
Shane Qi <qizengtai@gmail.com>
2017-10-09 17:08:13 -0500

    Initial Commit

---
+++ SQActionSheet/SQActionSheet.h
@@ -0,0 +1,19 @@
+//
+//  SQActionSheet.h
+//  SQActionSheet
+//
+//  Created by Shane Qi on 10/9/17.
+//  Copyright © 2017 Shane Qi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SQActionSheet.
+FOUNDATION_EXPORT double SQActionSheetVersionNumber;
+
+//! Project version string for SQActionSheet.
+FOUNDATION_EXPORT const unsigned char SQActionSheetVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SQActionSheet/PublicHeader.h>
+
+
