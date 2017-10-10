812a3a4589385ecfa7a431771b0b097af9d6c5c2
Shane Qi <qizengtai@gmail.com>
2017-10-10 09:25:12 -0500

    Fixed dependencies APIs.

--- SQActionSheet/ActionsSheetViewController.swift
+++ SQActionSheet/ActionsSheetViewController.swift
@@ -7,37 +7,36 @@
 //
 
 import UIKit
-import SQITools
 
 final class ActionsSheetViewController: UIViewController {
-
+	
 	@IBOutlet var tableView: UITableView!
 	var transitionView: UIView? { return presentationController?.containerView }
 	var backgroundOverlay = UIView()
-
+	
 	var sheetTitle: String?
 	var cancelText: String?
 	var actions: [Action] = []
-
+	
 	struct Action {
 		var image: UIImage?
 		var text: String
 		var type: ActionType
 		var handler: ((Action) -> Void)?
 	}
-
+	
 	enum ActionType {
 		case `default`, destructive
 	}
-
-	static func getInstance(sheetTitle: String?, cancelText: String?, actions: [Action]) -> Self {
-		let viewController = getInstance()
+	
+	static func getInstance(sheetTitle: String?, cancelText: String?, actions: [Action]) -> ActionsSheetViewController {
+		let viewController = (UIStoryboard.init(name: "ActionsSheet", bundle: Bundle.init(for: self)).instantiateInitialViewController() as? ActionsSheetViewController)!
 		viewController.sheetTitle = sheetTitle
 		viewController.cancelText = cancelText
 		viewController.actions = actions.reversed()
 		return viewController
 	}
-
+	
 	override func viewWillAppear(_ animated: Bool) {
 		super.viewWillAppear(animated)
 		if let transitionView = self.transitionView {
@@ -50,31 +49,31 @@ final class ActionsSheetViewController: UIViewController {
 			}
 		}
 	}
-
+	
 	override func viewDidLoad() {
 		super.viewDidLoad()
 		tableView.transform = CGAffineTransform(rotationAngle: .pi)
 	}
-
+	
 	override func viewWillDisappear(_ animated: Bool) {
 		super.viewWillDisappear(animated)
 		UIView.animate(withDuration: 0.25) { [unowned self] in
 			self.backgroundOverlay.alpha = 0
 		}
 	}
-
+	
 	@IBAction func didTapBackground() {
 		guard cancelText != nil else { return }
 		dismiss(animated: true, completion: nil)
 	}
-
+	
 	fileprivate enum CellType {
 		case title
 		case cancel
 		case action(Int)
 		case header
 	}
-
+	
 	fileprivate func cellType(at indexPath: IndexPath) -> CellType {
 		if cancelText != nil && indexPath.section == 0 {
 			return .cancel
@@ -89,14 +88,12 @@ final class ActionsSheetViewController: UIViewController {
 
 }
 
-extension ActionsSheetViewController: StoryboardInstantiatable {}
-
 extension ActionsSheetViewController: UITableViewDataSource {
-
+	
 	func numberOfSections(in tableView: UITableView) -> Int {
 		return cancelText != nil ? 2 : 1
 	}
-
+	
 	func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
 		switch cellType(at: IndexPath(row: 0, section: section)) {
 		case .cancel:
@@ -105,30 +102,34 @@ extension ActionsSheetViewController: UITableViewDataSource {
 			return actions.count + (sheetTitle != nil ? 1 : 0) + 1
 		}
 	}
-
+	
 	var separatorCellIdentifier: String { return "ActionsSheetSeparatorCell" }
 	var marginCellIdentifier: String { return "ActionsSheetMarginCell" }
-
+	
 	func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
 		var cell: UITableViewCell
 		switch cellType(at: indexPath) {
 		case .title:
-			let thisCell: ActionsSheetTitleCell = tableView.cook.dequeueReusableCell(for: indexPath)
+			let thisCell = (tableView.dequeueReusableCell(withIdentifier: ActionsSheetTitleCell.identifier, for: indexPath)
+				as? ActionsSheetTitleCell)!
 			thisCell.titleLabel.text = sheetTitle
 			cell = thisCell
 		case .cancel:
-			let thisCell: ActionsSheetCancelCell = tableView.cook.dequeueReusableCell(for: indexPath)
+			let thisCell = (tableView.dequeueReusableCell(withIdentifier: ActionsSheetCancelCell.identifier, for: indexPath)
+				as? ActionsSheetCancelCell)!
 			thisCell.titleLabel.text = cancelText
 			cell = thisCell
 		case .action(let index):
 			let action = actions[index]
 			if case .destructive = action.type {
-				let thisCell: ActionsSheetDestructiveCell = tableView.cook.dequeueReusableCell(for: indexPath)
+				let thisCell = (tableView.dequeueReusableCell(withIdentifier: ActionsSheetDestructiveCell.identifier, for: indexPath)
+					as? ActionsSheetDestructiveCell)!
 				thisCell.iconImageView.image = action.image
 				thisCell.titleLabel.text = action.text
 				cell = thisCell
 			} else {
-				let thisCell: ActionsSheetDefaultCell = tableView.cook.dequeueReusableCell(for: indexPath)
+				let thisCell = (tableView.dequeueReusableCell(withIdentifier: ActionsSheetDefaultCell.identifier, for: indexPath)
+					as? ActionsSheetDefaultCell)!
 				thisCell.iconImageView.image = action.image
 				thisCell.titleLabel.text = action.text
 				cell = thisCell
@@ -143,7 +144,7 @@ extension ActionsSheetViewController: UITableViewDataSource {
 }
 
 extension ActionsSheetViewController: UITableViewDelegate {
-
+	
 	func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
 		switch cellType(at: indexPath) {
 		case .title:
@@ -154,16 +155,16 @@ extension ActionsSheetViewController: UITableViewDelegate {
 			}
 		case .action(let index):
 			DispatchQueue.main.async { [unowned self] in
-				self.dismiss(animated: true, completion: { _ in
+				self.dismiss(animated: true) {
 					let action = self.actions[index]
 					action.handler?(action)
-				})
+				}
 			}
 		case .header:
 			return
 		}
 	}
-
+	
 	func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
 		switch cellType(at: indexPath) {
 		case .title:
@@ -178,7 +179,7 @@ extension ActionsSheetViewController: UITableViewDelegate {
 }
 
 extension ActionsSheetViewController: UIGestureRecognizerDelegate {
-
+	
 	func gestureRecognizer(_ gestureRecognizer: UIGestureRecognizer, shouldReceive touch: UITouch) -> Bool {
 		var view = touch.view
 		while view?.superview != nil {
